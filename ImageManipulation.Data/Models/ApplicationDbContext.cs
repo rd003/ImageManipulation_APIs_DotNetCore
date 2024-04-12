d89e8afd5d06838b952b26423e267bea86d39dd1
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-12 17:04:02 +0530

    Add project files.

---
+++ ImageManipulation.Data/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImageManipulation.Data.Models;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ImageManipulation.Data/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImageManipulation.Data.Models;
+
+[Table("Product")]
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string? ProductName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string? ProductImage { get; set; }
+}
